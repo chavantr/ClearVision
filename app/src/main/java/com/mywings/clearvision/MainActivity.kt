d677bd0d8ae3b6bd67d4d485f49dddaae5abb17a
Tatyabhau R. Chavan <tatyabhau.chavan@gmail.com>
2021-05-01 21:05:15 +0530

    Initial commit

---
+++ app/src/main/java/com/mywings/clearvision/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.mywings.clearvision
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val permissions =
+        arrayOf(Manifest.permission.CAMERA)
+    private val EXTERNAL_REQUEST = 1001
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) !== PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this, permissions,
+                EXTERNAL_REQUEST
+            )
+        } else {
+            val intent = Intent(this@MainActivity, ScanQRCodeActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            val intent = Intent(this@MainActivity, ScanQRCodeActivity::class.java)
+            startActivity(intent)
+        } else {
+            lblWarning.visibility = View.VISIBLE
+        }
+    }
+}
---
+++ app/src/main/java/com/mywings/clearvision/ScanQRCodeActivity.kt
@@ -0,0 +1,47 @@
+package com.mywings.clearvision
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.CodeScannerView
+import com.budiyev.android.codescanner.DecodeCallback
+
+class ScanQRCodeActivity : AppCompatActivity() {
+
+    private var mCodeScanner: CodeScanner? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.qr_code_scanner_activity)
+        val scannerView = findViewById<CodeScannerView>(R.id.scanner_view)
+        mCodeScanner = CodeScanner(this, scannerView)
+        mCodeScanner?.decodeCallback = DecodeCallback { result ->
+            runOnUiThread {
+                Log.d("test", result.toString())
+            }
+        }
+    }
+
+    private fun stopPreview() {
+        mCodeScanner?.releaseResources()
+    }
+
+    private fun startPreview() {
+        mCodeScanner?.startPreview()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startPreview()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        stopPreview()
+    }
+}
